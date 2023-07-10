f8a1ea577b82846b682fdbad0804480bccff4941
void2610 <izumisan2610@gmail.com>
2023-07-11 01:04:20 +0900

    auto scroll

--- src/components/PlayList.jsx
+++ src/components/PlayList.jsx
@@ -1,6 +1,7 @@
 import SimpleBarReact from 'simplebar-react';
 import { List, ListItem, ListItemText, ListItemAvatar, Avatar } from '@material-ui/core';
 import 'simplebar-react/dist/simplebar.min.css';
+import { useRef, useEffect } from 'react';
 
 const PlayList = ({
   tracks,
@@ -8,18 +9,31 @@ const PlayList = ({
   setTrackIndex,
   setCurrentTrack
 }) => {
+  const simpleBarRef = useRef();
+
+  useEffect(() => {
+    if (simpleBarRef.current) {
+      const scrollElement = simpleBarRef.current.getScrollElement();
+      const listItem = scrollElement.querySelector(`[data-index="${trackIndex}"]`);
+      if (listItem) {
+        scrollElement.scrollTop = listItem.offsetTop;
+      }
+    }
+  }, [trackIndex]);
+
   function setTrackByList(index) {
     setTrackIndex(index);
     setCurrentTrack(tracks[index]);
   }
 
   return (
-    <SimpleBarReact style={{ maxHeight: '420px' }}>
+    <SimpleBarReact style={{ maxHeight: '420px' }} ref={simpleBarRef}>
       <List>
         {tracks.map((track, index) => (
           <ListItem button key={index} onClick={() => setTrackByList(index)}
             className="listItem"
             style={{ backgroundColor: trackIndex === index ? 'rgb(35, 35, 35)' : 'transparent' }}
+            data-index={index}
           >
             <ListItemAvatar>
               <Avatar src={track.thumbnail} alt={track.title} />
